2a07c7103a0109cd8282eb407e941494279fd2df
VEBAMA Wanematou <aidavebama@gmail.com>
2023-06-16 09:53:56 +0200

    projets5

---
+++ page/authentification.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../style/style.css">
+   
+</head>
+<body>
+<div class="login-box">
+    <img src="../images/imgauthent.jpg" alt="">
+  <h2>S'authentifier</h2>
+  <form>
+    <div class="user-box">
+      <input type="email" name="" required="">
+      <label>Email</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required="">
+      <label>Password</label>
+    </div>
+    <div class="authenta">
+        <button>Connexion</button>
+    </div>
+  </form>
+</div>
+</body>
+</html>
